c8d869fd49cb6b70555fd508b0b951d60734d6b6
houssam37 <houssam.dolmi37@gmail.com>
2023-12-09 13:52:17 +0100

    last commit

--- Server/server.js
+++ Server/server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs/promises');
@@ -8,29 +7,25 @@ const app = express();
 const port = process.env.PORT || 3001;
 const formDataFilePath = './form-data.json';
 
-
 app.use(bodyParser.json());
 app.use(cors());
 
 app.post('/submit-form', async (req, res) => {
-    try {
-      const formData = req.body;
-      console.log('Received form data:', formData);
-  
-      // Simulate a server error for testing
-      if (formData.name === 'test') {
-        throw new Error('Simulated server error');
-      }
-  
-      // Rest of your code
-      res.json({ success: true, message: 'Form submitted successfully!' });
-    } catch (error) {
-      console.error('Error processing form data:', error);
-      res.status(500).json({ success: false, message: 'Internal Server Error' });
-    }
-  });
-    
-    app.listen(port, () => {
-      console.log(`Server is running on http://localhost:${port}`);
-    });
-    
+  try {
+    const formData = req.body;
+    console.log('Received form data:', formData);
+
+    // Log form data to a JSON file
+    await fs.writeFile(formDataFilePath, JSON.stringify(formData, null, 2));
+
+    // Rest of your code
+    res.json({ success: true, message: 'Form submitted successfully!' });
+  } catch (error) {
+    console.error('Error processing form data:', error);
+    res.status(500).json({ success: false, message: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
